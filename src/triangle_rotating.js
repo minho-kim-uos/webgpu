9d9752efd434fc8e317853df74e48a84a66bf902
Minho Kim <minhokim@uos.ac.kr>
2024-10-25 10:34:24 +0900

    .

--- src/triangle_rotating.js
+++ src/triangle_rotating.js
@@ -5,8 +5,9 @@ import {
 
 const loc_position = 1;
 
-const id_group_transformation = 0;
-const id_binding_model_matrix = 4;
+const id_group_transformation = 0;  // This should be assigned from 0, sequentially.
+                                 // https://toji.dev/webgpu-best-practices/bind-groups.html
+const id_binding_model_matrix = 3;
 
 function toRadian(deg) { return deg*Math.PI / 180.0; }
 
@@ -25,15 +26,64 @@ async function main() {
         format: preferredFormat,
     });
 
-    const {buffer:vertexBuffer, layout:layoutVertexBuffer, count:countVertices}
-        = initVertexBuffer(device);
+    const vertices = new Float32Array([
+         0.0, 0.8,
+        -0.8,-0.8,
+         0.8,-0.8,
+    ]);
+    const vertexBuffer = device.createBuffer({
+        label:"triangle vertices",
+        size: vertices.byteLength,
+        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    });
 
-    const {module:shaderModule, entry_vert, entry_frag} = initShaderModule(device);
+    device.queue.writeBuffer(vertexBuffer, 0, vertices);
 
-    const pipeline = initPipeline(device, preferredFormat, shaderModule, entry_vert, entry_frag, layoutVertexBuffer);
+    const layoutVertexBuffer = {
+        arrayStride: 8,
+        attributes: [{
+            format: "float32x2",
+            offset: 0,
+            shaderLocation: loc_position,
+        }],
+    };
+
+    const src_shader = `
+
+        @group(${id_group_transformation}) @binding(${id_binding_model_matrix}) var<uniform> mat_model: mat4x4<f32>;
+
+        @vertex fn main_vert(@location(${loc_position}) position: vec2f)
+            -> @builtin(position) vec4f {
+            return mat_model * vec4f(position, 0, 1);
+        }
+        @fragment fn main_frag() -> @location(0) vec4f {
+            return vec4f(1, 0, 0, 1);
+        }
+    `;
+    const shaderModule = device.createShaderModule({
+        label: "rotating triangle shader",
+        code:src_shader,
+    });
+
+    const pipeline = device.createRenderPipeline({
+        label: "rotating triangle pipeline",
+        layout: "auto",
+        vertex: {
+            module:shaderModule,
+            entryPoint: "main_vert",
+            buffers: [layoutVertexBuffer],
+        },
+        fragment: {
+            module:shaderModule,
+            entryPoint: "main_frag",
+            targets: [{
+                format: preferredFormat
+            }]
+        },
+    });
 
     const uniformBuffer = device.createBuffer({
-        size: 4 * 16, // 4x4 matrix
+        size: 4 * 4 * 4, // 4x4 matrix
         usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
     });
 
@@ -83,7 +133,7 @@ async function main() {
         passRender.setPipeline(pipeline);
         passRender.setVertexBuffer(0, vertexBuffer);
         passRender.setBindGroup(id_group_transformation, bindGroup);
-        passRender.draw(countVertices);
+        passRender.draw(3);
         passRender.end();
         device.queue.submit([encoder.finish()]);
 
@@ -93,69 +143,6 @@ async function main() {
     requestAnimationFrame(tick);
 }
 
-function initVertexBuffer(device) {
-    const vertices = new Float32Array([
-         0.0, 0.8,
-        -0.8,-0.8,
-         0.8,-0.8,
-    ]);
-    const buffer = device.createBuffer({
-        label:"triangle vertices",
-        size: vertices.byteLength,
-        usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-    });
-    device.queue.writeBuffer(buffer, 0, vertices);
-
-    const layoutVertexBuffer = {
-        arrayStride: 8,
-        attributes: [{
-            format: "float32x2",
-            offset: 0,
-            shaderLocation: loc_position,
-        }],
-    };
-    return {buffer:buffer, layout:layoutVertexBuffer, count:vertices.length/2};
-}
-
-function initShaderModule(device) {
-    const src_vert = `
-        @group(${id_group_transformation}) @binding(${id_binding_model_matrix}) var<uniform> mat_model: mat4x4<f32>;
-        @vertex fn main_vert(@location(${loc_position}) position: vec2f)
-            -> @builtin(position) vec4f {
-            return mat_model * vec4f(position, 0, 1);
-        }
-    `;
-    const src_frag = `
-        @fragment fn main_frag() -> @location(0) vec4f {
-            return vec4f(1, 0, 0, 1);
-        }
-    `;
-    const shaderModule = device.createShaderModule({
-        label: "rotating triangle shader",
-        code:src_vert + src_frag,
-    });
-    return {module:shaderModule, entry_vert:"main_vert", entry_frag:"main_frag"};
-}
-
-function initPipeline(device, canvasFormat, shaderModule, entry_vert, entry_frag, layoutVertexBuffer) {
-    const pipeline = device.createRenderPipeline({
-        label: "rotating triangle pipeline",
-        layout: "auto",
-        vertex: {
-            module:shaderModule,
-            entryPoint: entry_vert,
-            buffers: [layoutVertexBuffer],
-        },
-        fragment: {
-            module:shaderModule,
-            entryPoint: entry_frag,
-            targets: [{
-                format: canvasFormat
-            }]
-        },
-    });
-    return pipeline;
-}
 
 main();
 
